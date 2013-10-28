ddbf03f6c96011228a0f98e8e645cd19fa2643e2
elemeNT <shangjing853@gmail.com>
2013-10-28 16:46:23 +0800

    inputstream测试

--- NetTest/src/com/sj/URLconnecTest.java
+++ NetTest/src/com/sj/URLconnecTest.java
@@ -1,6 +1,14 @@
 package com.sj;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -13,17 +21,36 @@ import java.net.URLConnection;
 public class URLconnecTest {
 
 	public static void main(String[] args) {
-			String urlStr = "http://www.baidu.com";
+			String urlStr = "http://localhost/iTunes64Setup.exe";
 			URL url;
-			URLConnection urlConnection;
+			HttpURLConnection urlConnection;
 			try {
 				url = new URL(urlStr);
-				urlConnection = url.openConnection();
-				System.out.println(urlConnection.getContentLength());
+				OutputStream os;
+				InputStream is;
+				urlConnection = (HttpURLConnection)url.openConnection();
+				urlConnection.setConnectTimeout(5000); 
+				if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
+					long totalLenth = urlConnection.getContentLength();
+					System.out.println("total-size: "+totalLenth);
+					is = urlConnection.getInputStream();
+					long currentLenth = 0;
+					int count  = 0;
+					while(count == 0){
+						count  = is.available();
+					}
+					byte[] b = new byte[count];
+					while(currentLenth < totalLenth){
+						currentLenth += is.read(b);
+						System.out.println("currentLenth: "+currentLenth);
+					}
+				 
+				}
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
+			}finally{
 			}
 	}
 
