afc8cefc12d3ecb2ae5928403cf1de0c188ce27c
elemeNT <shangjing853@gmail.com>
2013-10-28 17:31:15 +0800

    下载文件成功

--- NetTest/src/com/sj/URLconnecTest.java
+++ NetTest/src/com/sj/URLconnecTest.java
@@ -3,6 +3,7 @@ package com.sj;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,49 +17,52 @@ import java.net.URLConnection;
 
 /**
  * 
- * @author shang
- *URLconnection 试用
+ * @author shang URLconnection 试用
  */
 public class URLconnecTest {
 
 	public static void main(String[] args) {
-			String urlStr = "http://localhost/iTunes64Setup.exe";
-			URL url;
-			HttpURLConnection urlConnection;
-			try {
-				url = new URL(urlStr);
-				OutputStream os;
-				InputStream is;
-				urlConnection = (HttpURLConnection)url.openConnection();
-				urlConnection.setConnectTimeout(5000); 
-				if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
-					long totalLenth = urlConnection.getContentLength();
-					System.out.println("total-size: "+totalLenth);
-					is = urlConnection.getInputStream();
-					FileOutputStream fileOutputStream = new FileOutputStream("D:/new.exe");
-					os = new BufferedOutputStream(fileOutputStream);
-					long currentLenth = 0;
-					int count  = 0;
-					int bite = 0;
-					while(count == 0){
-						count  = is.available();
-					}
+		String urlStr = "http://localhost/iTunes64Setup.exe";
+		URL url;
+		HttpURLConnection urlConnection;
+		try {
+			url = new URL(urlStr);
+			OutputStream os;
+			InputStream is;
+			urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setConnectTimeout(5000);
+			if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				long totalLenth = urlConnection.getContentLength();
+				System.out.println("total-size: " + totalLenth);
+				is = urlConnection.getInputStream();
+				FileOutputStream fileOutputStream = new FileOutputStream(
+						"D:/iTunes64Setup.exe");
+				long currentLenth = 0;
+				int count = 0;
+				int bite = 0;
+				while (count == 0) {
+					count = is.available();
+					System.out.println("count:"+count);
 					byte[] b = new byte[count];
-					while(currentLenth < totalLenth){
+					count = -1;
+					if (currentLenth < totalLenth) {
 						bite = is.read(b);
-						os.flush();
-						os.write(bite);
+						if(bite == -1){
+							fileOutputStream.flush();
+						}
+						fileOutputStream.write(b);
 						currentLenth += bite;
-						System.out.println("currentLenth: "+currentLenth);
+						System.out.println("currentLenth: " + currentLenth);
+						count = 0;
 					}
-				 
 				}
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}finally{
 			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+		}
 	}
 
 }
