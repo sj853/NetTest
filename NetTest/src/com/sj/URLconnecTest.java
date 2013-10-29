d2f96b2eece34f4fcb1f6ec78d406012ef955fa3
elemeNT <shangjing853@gmail.com>
2013-10-29 14:49:38 +0800

    ver2

--- NetTest/src/com/sj/URLconnecTest.java
+++ NetTest/src/com/sj/URLconnecTest.java
@@ -1,19 +1,11 @@
 package com.sj;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * 
@@ -21,38 +13,46 @@ import java.net.URLConnection;
  */
 public class URLconnecTest {
 
-	public static void main(String[] args) {
+	public static long totalLenth = 0; 
+	
+	
+
+	public static void download() {
+
 		String urlStr = "http://localhost/iTunes64Setup.exe";
 		URL url;
 		HttpURLConnection urlConnection;
+		InputStream is = null;
+		FileOutputStream fileOutputStream = null;
 		try {
 			url = new URL(urlStr);
-			OutputStream os;
-			InputStream is;
 			urlConnection = (HttpURLConnection) url.openConnection();
 			urlConnection.setConnectTimeout(5000);
 			if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				long totalLenth = urlConnection.getContentLength();
+				totalLenth = urlConnection.getContentLength();
 				System.out.println("total-size: " + totalLenth);
 				is = urlConnection.getInputStream();
-				FileOutputStream fileOutputStream = new FileOutputStream(
+				fileOutputStream = new FileOutputStream(
 						"D:/iTunes64Setup.exe");
 				long currentLenth = 0;
 				int count = 0;
 				int bite = 0;
 				while (count == 0) {
 					count = is.available();
-					System.out.println("count:"+count);
+					System.out.println("count:" + count);
 					byte[] b = new byte[count];
 					count = -1;
 					if (currentLenth < totalLenth) {
+						long startTime = System.currentTimeMillis();
 						bite = is.read(b);
-						if(bite == -1){
+						long useTime = System.currentTimeMillis() - startTime;
+						if (bite == -1) {
 							fileOutputStream.flush();
 						}
 						fileOutputStream.write(b);
 						currentLenth += bite;
-						System.out.println("currentLenth: " + currentLenth);
+						System.out.println("currentLenth: " + currentLenth
+								+ " speed: " );
 						count = 0;
 					}
 				}
@@ -62,7 +62,31 @@ public class URLconnecTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
+			try {
+				is.close();
+				fileOutputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+
+	}
+	
+	public static int sizeParse(long bitLen){
+		return (int) (bitLen/8/1024/1024);
+	}
+
+	public static void main(String[] args) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				long startTime = System.currentTimeMillis();
+				URLconnecTest.download();
+				long useTime = System.currentTimeMillis() - startTime;
+				System.out.println("total size : "+sizeParse(totalLenth)+" total use time: " + useTime
+						+ " avg speed: " + (URLconnecTest.totalLenth / useTime));
+			}
+		}).start();
 	}
 
 }
