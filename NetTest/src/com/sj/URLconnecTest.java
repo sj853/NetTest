9e685a6be54016806889c5642107ab7f1d26f480
elemeNT <shangjing853@gmail.com>
2013-10-30 16:51:41 +0800

    TimerTask

--- NetTest/src/com/sj/URLconnecTest.java
+++ NetTest/src/com/sj/URLconnecTest.java
@@ -6,6 +6,10 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.omg.PortableInterceptor.USER_EXCEPTION;
 
 /**
  * 
@@ -13,9 +17,12 @@ import java.net.URL;
  */
 public class URLconnecTest {
 
-	public static long totalLenth = 0; 
+	public static long totalLength = 0; 
 	
 	
+	static long currentLength = -1;
+	
+	static int currentUseTime = 0;
 
 	public static void download() {
 
@@ -29,12 +36,11 @@ public class URLconnecTest {
 			urlConnection = (HttpURLConnection) url.openConnection();
 			urlConnection.setConnectTimeout(5000);
 			if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				totalLenth = urlConnection.getContentLength();
-				System.out.println("total-size: " + totalLenth);
+				totalLength = urlConnection.getContentLength();
+				System.out.println("total-size: " + totalLength);
 				is = urlConnection.getInputStream();
 				fileOutputStream = new FileOutputStream(
 						"D:/iTunes64Setup.exe");
-				long currentLenth = 0;
 				int count = 0;
 				int bite = 0;
 				while (count == 0) {
@@ -42,16 +48,17 @@ public class URLconnecTest {
 					System.out.println("count:" + count);
 					byte[] b = new byte[count];
 					count = -1;
-					if (currentLenth < totalLenth) {
+					if (currentLength < totalLength) {
 						long startTime = System.currentTimeMillis();
 						bite = is.read(b);
 						long useTime = System.currentTimeMillis() - startTime;
+						currentUseTime += useTime;
 						if (bite == -1) {
 							fileOutputStream.flush();
 						}
 						fileOutputStream.write(b);
-						currentLenth += bite;
-						System.out.println("currentLenth: " + currentLenth
+						currentLength += bite;
+						System.out.println("currentLenth: " + currentLength
 								+ " speed: " );
 						count = 0;
 					}
@@ -75,6 +82,12 @@ public class URLconnecTest {
 	public static int sizeParse(long bitLen){
 		return (int) (bitLen/8/1024/1024);
 	}
+	
+	public static int secondeParse(long time){
+		return (int)(time/1000);
+	}
+	
+	
 
 	public static void main(String[] args) {
 		new Thread(new Runnable() {
@@ -83,10 +96,24 @@ public class URLconnecTest {
 				long startTime = System.currentTimeMillis();
 				URLconnecTest.download();
 				long useTime = System.currentTimeMillis() - startTime;
-				System.out.println("total size : "+sizeParse(totalLenth)+" total use time: " + useTime
-						+ " avg speed: " + (URLconnecTest.totalLenth / useTime));
+//				System.out.println("total size : "+sizeParse(totalLenth)+" total use time: " + secondeParse(useTime)
+//						+ " avg speed: " + (URLconnecTest.totalLenth / secondeParse(useTime)));
 			}
 		}).start();
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			int time = 0;
+			@Override
+			public void run() {
+				time++;
+				try {
+					System.out.println("currentSpeed: "+currentLength/time);
+				} catch (Exception e) {
+				}
+			}
+		},0, 1000);
+		
 	}
 
 }
